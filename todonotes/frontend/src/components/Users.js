0a006a8f5bb6898befa3b4c871b50920285f366a
Sergey <nsergeym@gmail.com>
2021-08-30 12:17:00 +0300

    Axios + Cors

---
+++ todonotes/frontend/src/components/Users.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const User = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+
+        </tr>
+
+    )
+}
+
+const UsersList = ({users}) => {
+    return (
+        <table>
+            <tr>
+                <th>Username</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+            </tr>
+            {users.map((user)=> <User user={user}/>)}
+
+        </table>
+    )
+}
+
+export default UsersList;
