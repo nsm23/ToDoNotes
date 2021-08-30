dd7b72d3227b67e18b8b8a6f205af89fe0ef5ed5
Sergey <nsergeym@gmail.com>
2021-08-30 10:20:47 +0300

    start react

---
+++ todonotes/frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = [];
+    }
+
+    render() {
+        return (
+            <div>Main App</div>
+        )
+    }
+}
+
+
+export default App;
